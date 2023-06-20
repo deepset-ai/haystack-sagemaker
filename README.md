097d7d6ba86f55f3820e5c67efbba18ff2d38add
Tuana Celik <tuana.celik@deepset.ai>
2023-06-20 19:07:32 +0200

    first iteration of the OS + Haystack setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# haystack-sagemaker
-This repo is a showcase of how you can use models deployed on AWS SageMaker in your Haystack Gen AI pipelines
+# Haystack Genertative QA Pipelines with SageMaker
+This repo is a showcase of how you can use models deployed on AWS SageMaker in your Haystack Gen AI pipelines.
+
+## The Data
+This showcase includes some documents we've crawled form the OpenSearch website and documentation pages. They've been indexed into an `OpenSearchDocumentStore` 
+with the `create_opensearch_index.py` script.
+The raw data is in the `/data` folder. Feel free to re-use them for your own testing and demos 🎉
+
+## The Model
+For this demo, we've deployed the falcon-40b-instruct model on AWS SageMaker with the JumpStart program which makes the deployment process really smooth.
---
+++ create_opensearch_index.py
@@ -0,0 +1,93 @@
+import json
+from haystack.nodes import PreProcessor
+from haystack.document_stores import OpenSearchDocumentStore
+from haystack.utils import launch_opensearch
+from os import listdir
+from os.path import isfile, join
+
+import urllib3
+urllib3.disable_warnings()
+
+
+def _opensearch_kwargs():
+  """Default arguments for OpenSearch"""
+  return {
+      'host': "localhost",
+      'port': 9200,
+      'verify_certs': False,
+      'scheme': "https",
+      'username': "admin",
+      'password': "admin",
+      'embedding_dim': 384,
+  }
+
+
+def build_doc_store() -> OpenSearchDocumentStore:
+  """Returns an instance of the OpenSearchDoc Store"""
+  return OpenSearchDocumentStore(
+      **_opensearch_kwargs()
+    )
+
+
+def read_json(basepath):
+    """Process already formatted JSON"""
+    files = [join(basepath, f) for f in listdir(basepath) if isfile(join(basepath, f))]
+    data = []
+    for file in files:
+        print(f"Ingesting: {file}")
+        with open(file, 'r') as f:
+            data.extend(json.load(f))
+    return data
+
+
+def format_doc(doc):
+    """Normalize doc to haystack format"""
+    content = doc.pop('content')
+    return {
+      'content': content, 
+      'meta': {**doc}
+    }
+
+
+def normalize_doc_list(document_list):
+    return [ format_doc(doc) for doc in document_list]
+
+
+def preprocess_data(data, preprocessor):
+    return preprocessor.process(data)
+
+
+def preprocessor_builder(split_length, split_overlap):
+    return PreProcessor (
+        clean_empty_lines=True, 
+        split_by='word',
+        split_respect_sentence_boundary=True,
+        split_length=split_length,
+        split_overlap=split_overlap
+    )
+
+
+def index_docs(docs):
+    """Push docs to docstore"""
+    docstore = build_doc_store()
+    docstore.write_documents(docs)
+
+
+def ingest_docs(split_length=100, split_overlap=20, basepath="data"):
+    """Read and normalize doc json before writing them to OpenSearch"""
+    raw_docs = read_json(basepath)
+    normalized = normalize_doc_list(raw_docs)
+    preprocessed = preprocess_data(
+                        normalized, 
+                        preprocessor_builder(
+                            split_length, 
+                            split_overlap))
+    
+    index_docs(preprocessed)
+
+
+if __name__=="__main__":
+    print("Launching OS")
+    launch_opensearch()
+    print("Ingesting docs")
+    ingest_docs()
